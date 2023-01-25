e46b063d5c1d0ce52ec5bfbdf6096b008dbfa1a4
AntônioCruz <106624745+Antonio880@users.noreply.github.com>
2023-01-25 10:19:20 -0300

    atualização

---
+++ JS/script-DOM.js
@@ -0,0 +1,26 @@
+/*let element = document.querySelector('h1');
+
+element.setAttribute('id', 'header');
+
+let header = document.querySelector('#header');
+
+console.log(header.getAttribute('class'));
+
+element.removeAttribute('id');
+
+let element = document.querySelector('body');
+
+element.style.backgroundColor = 'lightblue';
+
+element.classList.toogle('active')se tiver o active, ele ira remover, se não tiver, irá adicionar;
+
+console.log(element.classList)
+
+let body = document.querySelector('body');
+
+console.log(body.firstElementChild)*/
+
+let el = document.querySelector('body h1')
+
+console.log(el.nextElementSibling)
+console.log(el.previousElementSibling)
